67a559ba583ed205a60b0ce98d5883420fce8c35
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-07-06 21:25:51 +0200

    [test] Improve React performance track tests

--- test/development/app-dir/react-performance-track/app/layout.tsx
+++ test/development/app-dir/react-performance-track/app/layout.tsx
@@ -1,9 +1,14 @@
 import { ReactNode, Suspense } from 'react'
+import { ReactServerRequests } from './ReactServerRequests'
 export default function Root({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
-        <Suspense>{children}</Suspense>
+        <ReactServerRequests />
+        <Suspense fallback="Loading Server Requests">
+          <div data-react-server-requests-done />
+          {children}
+        </Suspense>
       </body>
     </html>
   )
