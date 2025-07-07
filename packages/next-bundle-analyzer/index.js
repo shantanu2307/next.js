25ca4bfdef681cc9e61f546eb0b58aeb5e9ac3df
Shantanu Goyal <59186786+shantanu2307@users.noreply.github.com>
2025-07-07 19:07:46 +0530

    fix: file extension of report

--- packages/next-bundle-analyzer/index.js
+++ packages/next-bundle-analyzer/index.js
@@ -17,7 +17,7 @@ module.exports =
               ? `./analyze/client.html`
               : `../${options.nextRuntime === 'nodejs' ? '../' : ''}analyze/${
                   options.nextRuntime
-                }.html`,
+                }${analyzerMode==="json" ? "json":"html"}`,
           })
         )
 
